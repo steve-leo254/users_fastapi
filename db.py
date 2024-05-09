331900ea25da5a0cec6e9956cceb2a57b897f4e4
steve-leo254 <steveleo254@gmail.com>
2024-05-09 11:59:11 +0300

    5/9/2024

--- db.py
+++ db.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float, DateTime
-from sqlalchemy.orm import declarative_base, sessionmaker, relationship,Session
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship, Session
 from datetime import datetime, timezone
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:leo.steve@localhost:5432/sale_system_api'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()  
+Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
@@ -24,25 +24,23 @@ class Product(Base):
     name = Column(String, nullable=False)
     cost = Column(Float, nullable=False)
     price = Column(Float, nullable=False)
-    created_at = Column(
-        DateTime, default=datetime.now(timezone.utc), nullable=False)  
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     stock_quantity = Column(Integer, default=0)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
 
     user = relationship("User", back_populates="products")
 
-
 class Sale(Base):
-    __tablename__ = "sale"
+    __tablename__ = 'sale'
+    id = Column(Integer, primary_key=True)
+    total_amount = Column(Float, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    Pid = Column(Integer,ForeignKey("products.id"), nullable=False)
-    quantity = Column(Integer, nullable=False)
-    created_at = Column(
-        DateTime, default=datetime.now(timezone.utc), nullable=False) 
-    Product = relationship("Product",back_populates="sales" )
-
-
-
+class Customers(Base):
+    __tablename__ = "customers"  # Fixed typo here
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    phone_no = Column(String, nullable=False)
+    email = Column(String, nullable=False)
 
 Base.metadata.create_all(bind=engine)
